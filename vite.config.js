f1717934d9841ca6698f34aea9bc63ce01a574ae
ikgagan <ik.gagan@gmail.com>
2024-11-02 19:02:06 -0700

    deploy changes

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { ghPages } from 'vite-plugin-gh-pages';
 
-// https://vite.dev/config/
+// Replace 'your-username' and 'your-repo-name' with your actual GitHub username and repository name
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), ghPages()],
+  base: 'portfolio-aanwi', // Set this to the name of your repository
+});
